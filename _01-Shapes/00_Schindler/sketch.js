6a6b6c4ddbbf876fbd2998bc73654fc9b97f03bb
Merc <mc.hausler@hotmail.com>
2019-12-13 00:00:05 +0100

    Revert "fix space sketch + directory rework"
    This reverts commit 7275db31a13f919f38801f6c0fea7970912bf8c0.

---
+++ _01-Shapes/00_Schindler/sketch.js
@@ -0,0 +1,140 @@
+var stars = [];
+var shootingStar;
+var moon;
+var drawMode;
+var pg;
+
+function setup() {
+	pixelDensity(1);
+  createCanvas(windowWidth, windowHeight);
+  frameRate(60);
+  for (var i = 0; i < 100; i++) {
+    stars.push(new Star());
+  }
+  shootingStar = new ShootingStar();
+  moon = new Moon();
+  pg = createGraphics(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  var color1 = color(0, 0, 153);
+  var color2 = color(204, 51, 0);
+  setGradient(0, 0, windowWidth, windowHeight, color1, color2);
+
+  for (var i = 0; i < 50; i++) {
+    stars[i].draw();
+  }
+  shootingStar.draw();
+  moon.draw();
+  rotateSky(drawMode);
+  drawMountain();
+  image(pg, 0, windowHeight/2, 0, 0);
+}
+
+function drawMountain(){
+  pg.noStroke();//stroke(255);
+  pg.fill(180,180,180);
+  pg.beginShape();
+  for (var x = 0; x < width; x++) {
+	var nx = map(x, 0, width, 0, 10);
+	var y = (windowHeight/2) * noise(nx);
+	pg.vertex(x, y/2);
+  }
+  pg.vertex(windowWidth, windowHeight);
+  pg.vertex(0,windowHeight);
+  pg.endShape();
+}
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+    for (let i = y; i <= y + h; i++) {
+      var inter = map(i, y, y + h, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }  
+}
+
+function Star() {
+  this.x = random(windowWidth);
+  this.y = random(windowHeight + 400) - 400;
+  this.w = 5;//windowHeight / 200;
+  this.h = 5;//windowHeight / 200;
+}
+
+Star.prototype.draw = function() {
+  noStroke();
+  fill(255, 255, 0);
+  ellipse(this.x, this.y, this.w, this.h);
+  if (this.w == 5 && frameCount % 5 == 0) {
+    this.w = 7;
+    this.h = 7;
+  } else if(this.w == 7 && frameCount % 5 == 0){
+    this.w = 5;
+    this.h = 5;
+  }
+}
+
+function ShootingStar() {
+  this.x = random(windowWidth - 200);
+  this.y = random(windowHeight - 400);
+  this.w = 20;
+  this.h = 5;
+}
+
+ShootingStar.prototype.draw = function() {
+  noStroke();
+  fill(255, 255, 0);
+  ellipse(this.x, this.y, this.w, this.h);
+  if (this.h > 0) {
+    this.h -= 0.2;
+  }
+  this.w += 7;
+  this.x += 5;
+}
+
+function Moon() {
+  this.x = windowWidth / 2;
+  if(drawMode == 2){
+	this.y = windowHeight/5;
+	} 
+  else {
+	  this.y = windowHeight - windowHeight/3;
+	}
+  this.w = windowHeight / 10;
+  this.h = windowHeight / 10;
+}
+
+Moon.prototype.draw = function() {
+  noStroke();
+  fill(255, 255, 0);
+  ellipse(this.x, this.y, this.w, this.h);
+  //this.y += 1;
+}
+
+function keyPressed() {
+
+  if (keyCode === 32) setup() // 32 = Space
+  if (keyCode === 38) drawMode = 2 // 38 = ArrowUp
+  if (keyCode === 40) drawMode = 1 // 40 = ArrowDown
+
+}
+
+function rotateSky(drawMode){
+	if(drawMode == 2){
+		moon.y += 1;
+		for (var i = 0; i < 50; i++) {
+			stars[i].y += 1;
+		}
+	}
+	else if(drawMode == 1){
+		moon.y -= 1;
+		for (var i = 0; i < 50; i++) {
+			stars[i].y -= 1;
+		}
+	}
+	else {
+		// do nothing
+	}
+}
--- _01-Shapes/01_01/sketch.js
+++ _01-Shapes/01_01/sketch.js
@@ -1,140 +1,84 @@
-var stars = [];
-var shootingStar;
-var moon;
-var drawMode;
-var pg;
+// Based on the code P_2_0_02.pde from
+// Generative Gestaltung, ISBN: 978-3-87439-759-9
 
+// Global var
+var b = 255, p = false;
+ 
 function setup() {
-	pixelDensity(1);
-  createCanvas(windowWidth, windowHeight);
-  frameRate(60);
-  for (var i = 0; i < 100; i++) {
-    stars.push(new Star());
-  }
-  shootingStar = new ShootingStar();
-  moon = new Moon();
-  pg = createGraphics(windowWidth, windowHeight);
+  // Canvas setup
+  canvas = createCanvas(windowWidth, windowHeight-45);
+  canvas.parent("p5Container");
+  // Detect screen density (retina)
+  var density = displayDensity();
+  pixelDensity(density);
+  // Colors and drawing modes
+  background(255);
+  smooth();
+  // Init Var
 }
 
 function draw() {
-  background(220);
-  var color1 = color(0, 0, 153);
-  var color2 = color(204, 51, 0);
-  setGradient(0, 0, windowWidth, windowHeight, color1, color2);
-
-  for (var i = 0; i < 50; i++) {
-    stars[i].draw();
-  }
-  shootingStar.draw();
-  moon.draw();
-  rotateSky(drawMode);
-  drawMountain();
-  image(pg, 0, windowHeight/2, 0, 0);
-}
-
-function drawMountain(){
-  pg.noStroke();//stroke(255);
-  pg.fill(180,180,180);
-  pg.beginShape();
-  for (var x = 0; x < width; x++) {
-	var nx = map(x, 0, width, 0, 10);
-	var y = (windowHeight/2) * noise(nx);
-	pg.vertex(x, y/2);
-  }
-  pg.vertex(windowWidth, windowHeight);
-  pg.vertex(0,windowHeight);
-  pg.endShape();
-}
-
-function setGradient(x, y, w, h, c1, c2, axis) {
+  smooth();
   noFill();
-    for (let i = y; i <= y + h; i++) {
-      var inter = map(i, y, y + h, 0, 1);
-      var c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(x, i, x + w, i);
-    }  
-}
 
-function Star() {
-  this.x = random(windowWidth);
-  this.y = random(windowHeight + 400) - 400;
-  this.w = 5;//windowHeight / 200;
-  this.h = 5;//windowHeight / 200;
-}
+  if (p) {
+    b = random(255);
+    push();
 
-Star.prototype.draw = function() {
-  noStroke();
-  fill(255, 255, 0);
-  ellipse(this.x, this.y, this.w, this.h);
-  if (this.w == 5 && frameCount % 5 == 0) {
-    this.w = 7;
-    this.h = 7;
-  } else if(this.w == 7 && frameCount % 5 == 0){
-    this.w = 5;
-    this.h = 5;
+    translate(width / 2, height / 2);
+
+    var circleResolution = toInt(map(mouseY + 100, 0, height, 2, 10));
+    var radius = mouseX - width / 2 + 0.5;
+    var angle = TWO_PI / circleResolution;
+
+    strokeWeight(2);
+    stroke(b, 25);
+
+    beginShape();
+    for (i = 0; i <= circleResolution; i++) {
+      var x = 0 + cos(angle * i) * radius;
+      var y = 0 + sin(angle * i) * radius;
+      vertex(x, y);
+    }
+    endShape();
+
+    pop();
   }
 }
 
-function ShootingStar() {
-  this.x = random(windowWidth - 200);
-  this.y = random(windowHeight - 400);
-  this.w = 20;
-  this.h = 5;
+function mousePressed() {
+  p = true;
 }
 
-ShootingStar.prototype.draw = function() {
-  noStroke();
-  fill(255, 255, 0);
-  ellipse(this.x, this.y, this.w, this.h);
-  if (this.h > 0) {
-    this.h -= 0.2;
-  }
-  this.w += 7;
-  this.x += 5;
-}
-
-function Moon() {
-  this.x = windowWidth / 2;
-  if(drawMode == 2){
-	this.y = windowHeight/5;
-	} 
-  else {
-	  this.y = windowHeight - windowHeight/3;
-	}
-  this.w = windowHeight / 10;
-  this.h = windowHeight / 10;
-}
-
-Moon.prototype.draw = function() {
-  noStroke();
-  fill(255, 255, 0);
-  ellipse(this.x, this.y, this.w, this.h);
-  //this.y += 1;
+function mouseReleased() {
+  p = false;
 }
 
 function keyPressed() {
-
-  if (keyCode === 32) setup() // 32 = Space
-  if (keyCode === 38) drawMode = 2 // 38 = ArrowUp
-  if (keyCode === 40) drawMode = 1 // 40 = ArrowDown
-
+  // Clear sketch
+  if (keyCode === 32) background(255) // 32 = SPACE BAR 
+  if (key == 's' || key == 'S') saveThumb(650, 350);
 }
 
-function rotateSky(drawMode){
-	if(drawMode == 2){
-		moon.y += 1;
-		for (var i = 0; i < 50; i++) {
-			stars[i].y += 1;
-		}
-	}
-	else if(drawMode == 1){
-		moon.y -= 1;
-		for (var i = 0; i < 50; i++) {
-			stars[i].y -= 1;
-		}
-	}
-	else {
-		// do nothing
-	}
+// Tools
+
+// resize canvas when the window is resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight, false);
+}
+
+// Int conversion
+function toInt(value) {
+  return ~~value;
+}
+
+// Timestamp
+function timestamp() {
+  return Date.now();
+}
+
+// Thumb
+function saveThumb(w, h) {
+  let img = get( width/2-w/2, height/2-h/2, w, h);
+  save(img,'thumb.jpg');
 }
