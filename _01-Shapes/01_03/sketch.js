b2788d10f5bdcd966914937fdd94c973607867ab
Merc <mc.hausler@hotmail.com>
2019-12-13 10:11:31 +0100

    added direction change

--- _01-Shapes/01_03/sketch.js
+++ _01-Shapes/01_03/sketch.js
@@ -1,7 +1,7 @@
 
 // Global var
 var canvas;
- 
+
 var lines = [];
 var planets = [];
 var overallIndexLines;
@@ -10,6 +10,7 @@ var firstCreationLine;
 var firstCreationPlanet;
 var speed;
 var lineLength;
+var direction;
 
 function setup() {
   // Canvas setup and variables
@@ -22,6 +23,7 @@ function setup() {
   firstCreationPlanet = true;
   lineLength = height * 2;// / 3 * 2;
   speed = 20;
+  direction = "up";
 
   // drawing modes
   frameRate(30);
@@ -35,7 +37,7 @@ function setup() {
     }
     else {
       lines.push(new randLine(true));
-    }  
+    }
   }
 }
 
@@ -56,12 +58,14 @@ function keyPressed() {
   // Clear sketch
   if (keyCode === 32) background(255) // 32 = SPACE BAR 
   if (key == 's' || key == 'S') saveThumb(width, height);
+  if (keyCode === 38) direction = "up"; // 38 = ArrowUp
+  if (keyCode === 40) direction = "down"; // 40 = ArrowDown
 }
 
 // Thumb
 function saveThumb(w, h) {
-  let img = get( width/2-w/2, height/2-h/2, w, h);
-  save(img,'thumb.jpg');
+  let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
+  save(img, 'thumb.jpg');
 }
 
 class randLine {
@@ -69,37 +73,70 @@ class randLine {
     this.arIndex = overallIndexLines;
     overallIndexLines++;
     if (firstIteration) {
-      this.x1 = int(random(width));
-      this.x2 = this.x1;
-      this.y1 = int(random(height * 2));
-      this.y2 = this.y1 + lineLength;
+      if (direction == "up") {
+        this.x1 = int(random(width));
+        this.x2 = this.x1;
+        this.y1 = int(random(height * 2));
+        this.y2 = this.y1 + lineLength;
+      }
+      else if (direction == "down") {
+        this.x1 = int(random(width));
+        this.x2 = this.x1;
+        this.y1 = 0 - int(random(height * 2));
+        this.y2 = this.y1 - lineLength;
+      }
+      
     } else {
-      this.x1 = int(random(width));
-      this.x2 = this.x1;
-      this.y1 = height + int(random(height));;
-      this.y2 = this.y1 + lineLength;
+      if (direction == "up") {
+        this.x1 = int(random(width));
+        this.x2 = this.x1;
+        this.y1 = height + int(random(height));;
+        this.y2 = this.y1 + lineLength;
+      }
+      else if (direction == "down") {
+        this.x1 = int(random(width));
+        this.x2 = this.x1;
+        this.y1 = 0 - int(random(height));;
+        this.y2 = this.y1 - lineLength;
+      }
     }
-    lastXPos = this.x1;
+    //lastXPos = this.x1;
   }
 
-// Tools
+  // Tools
   move() {
-    this.y1 -= speed;
-    this.y2 -= speed;
-    
-    if (this.y2 < 0) {
+    if (direction == "up") {
+      this.y1 -= speed;
+      this.y2 -= speed;
+
+      if (this.y2 < 0) {
         lines.splice(this.arIndex, 1);
         firstCreationLine = false;
         for (let index = this.arIndex; index < lines.length; index++) {
-          lines[index].arIndex--;          
+          lines[index].arIndex--;
         }
-      overallIndexLines--;
-      lines.push(new randLine(false));    
+        overallIndexLines--;
+        lines.push(new randLine(false));
+      }
     }
-    
+    else if (direction == "down") {
+      this.y1 += speed;
+      this.y2 += speed;
+
+      if (this.y2 > height) {
+        lines.splice(this.arIndex, 1);
+        firstCreationLine = false;
+        for (let index = this.arIndex; index < lines.length; index++) {
+          lines[index].arIndex--;
+        }
+        overallIndexLines--;
+        lines.push(new randLine(false));
+      }
+    }
+
   }
 
-  display(){
+  display() {
     line(this.x1, this.y1, this.x2, this.y2);
   }
 }
@@ -110,41 +147,69 @@ class Planet {
     this.arIndex = overallIndexPlanets;
     overallIndexPlanets++;
     if (firstIteration) {
-      this.xPos = int(random(width));
-      this.yPos = height + int(random(10000));
-      this.diameter = random(100, 300);
-      
+      if (direction == "up") {
+        this.xPos = int(random(width));
+        this.yPos = height + int(random(10000));
+        this.diameter = random(100, 300);
+      } 
+      else if (direction == "down") {
+        this.xPos = int(random(width));
+        this.yPos = 0 - int(random(10000));
+        this.diameter = random(100, 300);
+      }      
+
     } else {
-      this.xPos = int(random(width));
-      this.yPos = height + int(random(10000));
-      this.diameter = random(100, 300);
-    }
-  }
-
-  move(){
-    this.yPos -= speed;
-
-    if (this.yPos + this.diameter < 0) {
-      planets.splice(this.arIndex, 1);
-      firstCreationPlanet = false;
-      for (let index = this.arIndex; index < planets.length; index++) {
-        planets[index].arIndex--;          
+      if (direction == "up") {
+        this.xPos = int(random(width));
+        this.yPos = height + int(random(10000));
+        this.diameter = random(100, 300);
       }
-    overallIndexPlanets--;
-    planets.push(new Planet(false));    
+      else if (direction == "down") {
+        this.xPos = int(random(width));
+        this.yPos = 0 - int(random(10000));
+        this.diameter = random(100, 300);
+      }
+      
     }
   }
 
-  display(){
+  move() {
+    if (direction == "up") {
+      this.yPos -= speed;
+      if (this.yPos + this.diameter < 0) {
+        planets.splice(this.arIndex, 1);
+        firstCreationPlanet = false;
+        for (let index = this.arIndex; index < planets.length; index++) {
+          planets[index].arIndex--;
+        }
+        overallIndexPlanets--;
+        planets.push(new Planet(false));
+      }
+    }
+    else if (direction == "down") {
+      this.yPos += speed;
+      if (this.yPos - this.diameter > height) {
+        planets.splice(this.arIndex, 1);
+        firstCreationPlanet = false;
+        for (let index = this.arIndex; index < planets.length; index++) {
+          planets[index].arIndex--;
+        }
+        overallIndexPlanets--;
+        planets.push(new Planet(false));
+      }
+    }
+  }
+
+  display() {
     noStroke();
     fill(255);
     ellipse(this.xPos, this.yPos, this.diameter);
     fill(0);
-    ellipse(this.xPos+5, this.yPos+7, this.diameter);
+    ellipse(this.xPos + 5, this.yPos + 7, this.diameter);
   }
 }
 // Thumb
 function saveThumb(w, h) {
-  let img = get( width/2-w/2, height/2-h/2, w, h);
-  save(img,'thumb.jpg');
+  let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
+  save(img, 'thumb.jpg');
 }
